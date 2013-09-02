6885d37c238f84c3cd31245e3b2be57b6577d4f8
unknown <eldercerrato93@gmail.com>
2013-09-02 12:51:53 -0600

    se agrego prueba para probar cadena vacias

---
+++ kata.specs/cuando_envio_una_cadena_vacia.cs
@@ -0,0 +1,23 @@
+﻿using Machine.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata.specs
+{
+    public class cuando_envio_una_cadena_vacia
+    {
+        static Calculadora calculadora;
+        static int _resultado;
+        Establish contexto = () => 
+        {
+            calculadora = new Calculadora();
+        };
+
+        Because of = () => _resultado = calculadora.Sumar("");
+
+        It deberia_retornar_0 = () => _resultado.ShouldEqual(0);
+    }
+}
--- kata/Calculadora.cs
+++ kata/Calculadora.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace kata.specs
+namespace kata
 {
-    public class Class1
+    public class Calculadora
     {
+        public int Sumar(string cadena) 
+        {
+            return -1;
+        }
     }
 }
