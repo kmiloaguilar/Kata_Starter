a0986492e528e1b316d42248666b88f17cd44a72
unknown <saulmadi@gmail.com>
2013-09-06 10:14:07 -0600

    To Richard

---
+++ kata.specs/FizzBuzz/when_getting_a_number_containing_3.cs
@@ -0,0 +1,16 @@
+﻿using Machine.Specifications;
+
+namespace kata.specs.FizzBuzz
+{
+    public class when_getting_a_number_containing_3:given_a_fizz_buzzer_context
+    {
+        static string _result;
+        Because of =
+            () => _result = FizzBuzzer.Play(13);
+
+        It should_return_string_fizz =
+            () => _result.ShouldEqual("Fizz");
+
+        
+    }
+}
